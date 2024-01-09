3f8562306a335ce9811346919f9ffd799a8c9f5c
Ipfani Muleya <muleyaipfani22@gmail.com>
2024-01-09 12:47:45 -0800

    A function that reads a text file (UTF8) and prints it to stdout + README

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for reading a text file (UTF8) and printing to stdout."""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read())
